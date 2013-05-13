402a0d9092181249428dc188810fa4af8df80d2d
Jeremy Whitbred <pazrul@gmail.com>
2013-05-13 16:47:53 -0700

    First scaffold deploy, starting up

---
+++ README.md
@@ -0,0 +1,4 @@
+This is a rails test, something to play with. 
+
+Also something to break, nothing of consequence other than I need a place to store some code.
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = 'ee480baa69ff23aff299d54a9021294455c5748ac18909c87d23099969708eda6c61ab181e8d4fada632028c3c09bc037a10aa979b26bb8cba2795afd4de908a'
